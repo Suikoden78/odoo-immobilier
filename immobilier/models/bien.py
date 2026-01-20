2d1d544fcd175a170892a52f486f1899ceada898
Laurent F. <suikoden69@hotmail.com>
2026-01-20 14:41:03 +0100

    Correction du modèle et des vues

--- immobilier/models/bien.py
+++ immobilier/models/bien.py
@@ -4,7 +4,9 @@ class BienImmobilier(models.Model):
     _name = "immobilier.bien"
     _description = "Bien immobilier"
 
-    name = fields.Char(string="Nom du bien", required=True)
-    prix = fields.Float(string="Prix")
-    surface = fields.Float(string="Surface (m²)")
-    disponible = fields.Boolean(string="Disponible", default=True)
+    x_bien_titre_du_bien = fields.Char(string="Titre du bien", required=True)
+    x_bien_reference = fields.Char(string="Référence")
+    x_bien_statut = fields.Char(string="Statut")
+    x_bien_prix_du_bien = fields.Float(string="Prix")
+    x_bien_surface_habitable = fields.Float(string="Surface habitable (m²)")
+    x_bien_surface_du_terrain = fields.Float(string="Surface du terrain (m²)")
