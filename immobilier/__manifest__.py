3aeac2a594ed6ffb255b3c1e2e008a148d641095
Laurent F. <suikoden69@hotmail.com>
2026-01-21 14:07:49 +0100

    Correctif du manifest

--- immobilier/__manifest__.py
+++ immobilier/__manifest__.py
@@ -5,11 +5,12 @@
     'description': 'Module permettant de gérer les annonces de biens immobiliers.',
     'author': 'Francois Laurent',
     'category': 'Real Estate',
-    'depends': ['base'],
+    'depends': ['base','website_sale'],
     'data': [
         'security/ir.model.access.csv',
         'views/bien_view.xml',
     ],
     'installable': True,
     'application': True,
+    'license': 'LGPL-3',
 }
